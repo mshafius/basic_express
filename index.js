2ce797e65254fe04e46b2b50a50210e9c23a45e6
salim <salimshafiumuhd2002@gmail.com>
2023-12-22 00:07:27 +0100

    adding more url path

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 
 const app = express()
 const port = 3000
+
+//basic hompage
 app.get('/', (req, res) => {
     res.send("Hello, World!")
+})
+
+//about page
+app.get('/about', (req, res) => {
+    res.send("About Us")
 })
 
 
