58bd1b1c8d23726701a722294bac8eb055995366
salim <salimshafiumuhd2002@gmail.com>
2023-12-22 01:36:49 +0100

    commit some important updates

---
+++ Members.js
@@ -0,0 +1,23 @@
+//local datas
+const members = [
+    {
+        id: 1,
+        name: "Faruk Shafiu",
+        email: "farukshafiu@gmail.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "Usman Muhammad",
+        email: "usmanmuhd@gmail.com",
+        status: "inactive"
+    },
+    {
+        id: 3,
+        name: "Umar Muhammad",
+        email: "umarruss@gmail.com",
+        status: "active"
+    }
+]
+
+module.exports = members
--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express')
+const path = require('path')
+const logger = require('./middleware/Logger')
+const members = require('./Members')
 
-const app = express()
+
+// init express framework
+const app = express() 
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+// PORT
 const port = 3000
 
-//basic hompage
-app.get('/', (req, res) => {
-    res.send("Hello, World!")
-})
+// init middleware
+app.use(logger)
 
-//about page
-app.get('/about', (req, res) => {
-    res.send("About Us")
-})
 
+//Get All members
+app.get('/api/members', (req, res) => res.json(members))
 
 app.listen(port, ()=>{
     console.log(`Server listening at port ${port}`)
