55dc833761df51553c3a1b520aa204be6420e0d4
salim <salimshafiumuhd2002@gmail.com>
2023-12-22 13:56:54 +0100

    add detail and list api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const logger = require('./middleware/Logger')
-const members = require('./Members')
+const members = require('Members')
 
 
 // init express framework
@@ -16,8 +16,26 @@ const port = 3000
 app.use(logger)
 
 
-//Get All members
+//List View
 app.get('/api/members', (req, res) => res.json(members))
+
+
+//Detail View
+app.get('/api/members/:id', (req, res) => {
+    
+    //check if user exist
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    //if user exist
+    if (found){
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    }
+    else{
+        res.status(400).json({msg: "User does not exist!"})
+    }
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`Server listening at port ${port}`)
